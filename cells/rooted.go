f381844cc96855b96d6c213f10f496c578fcc55c
Matt Holden <holden.matt@gmail.com>
2018-04-20 18:37:40 +0900

    Move MinRoots to caplib from capsearch

--- cells/rooted.go
+++ cells/rooted.go
@@ -1,5 +1,7 @@
 package cells
 
+import "log"
+
 // Rooted represents a Cells in which we have selected points in the origin cell.
 // We generate caps in a single cell compatible with the root.
 type Rooted struct {
@@ -38,4 +40,81 @@ func (r *Rooted) extendBits(bits Bits32, size int, elim Bits32, out []Bits32) []
 		}
 	}
 	return out
+}
+
+// MinRoots finds all symmetric root caps in the origin cell that are
+// minimal in their isomorphism class (effective up to CDim = 3).
+func (c Cells) MinRoots() []Rooted {
+	if c.CSpace.D > 3 {
+		panic("MinRoots only computable for CDim <= 3")
+	}
+
+	target := c.Counts[0]
+	caps := c.rootCaps(target)
+	log.Printf("# of roots in cell 0: %d", len(caps))
+
+	var res []Rooted
+	for _, root := range caps {
+		if c.isMinRoot(root) {
+			rooted := Rooted{c, root}
+			res = append(res, rooted)
+		}
+	}
+	log.Printf("# of unique roots: %d", len(res))
+
+	return res
+}
+
+// rootCaps returns all symmetric caps of a given size in the origin cell.
+func (c Cells) rootCaps(size int) []Bits32 {
+	if (size % 2) != 0 {
+		panic("size must be even")
+	}
+
+	empty := []Bits32{Bits32(0)}
+	nPairs := size / 2
+	return c.addPairs(empty, nPairs)
+}
+
+// addPairs adds nPairs pairs to the given caps, and returns all resulting caps.
+func (c Cells) addPairs(caps []Bits32, nPairs int) []Bits32 {
+	if nPairs == 0 {
+		return caps
+	}
+
+	prevCaps := c.addPairs(caps, nPairs-1)
+	directions := c.CSpace.Directions
+
+	var nextCaps []Bits32
+	for _, cap := range prevCaps {
+		// Skip any eliminated pts; only extend by larger directions.
+		elim := c.EliminatedFast(cap, cap)
+		maxDir := cap.Intersection(directions).Maximum()
+
+		for _, p := range directions {
+			if p > maxDir && !elim.Test(p) {
+				nextCap := cap.Set(p).Set(c.CSpace.Inv[p])
+				nextCaps = append(nextCaps, nextCap)
+			}
+		}
+	}
+	return nextCaps
+}
+
+// isMinRoot returns true iff root is minimal in its isomorphism class.
+func (c Cells) isMinRoot(root Bits32) bool {
+	var im1, im2 Bits32
+
+	for _, perm1 := range c.CIsoms.Perms1.Perms {
+		im1 = root.Apply(perm1)
+
+		for _, perm2 := range c.CIsoms.Perms2.Perms {
+			im2 = im1.Apply(perm2)
+
+			if im2.Less(root) {
+				return false
+			}
+		}
+	}
+	return true
 }
--- cells/rooted_test.go
+++ cells/rooted_test.go
@@ -22,3 +22,20 @@ func ExampleRooted_CellCaps() {
 	// [1 2 4 8 16 32 64 128 256]
 	// [17 33 129 257 10 34 66 258 12 20 68 132 136 264 80 272 96 160]
 }
+
+func ExampleCells_MinRoots() {
+	c := New(space.New(4), []int{4, 2, 2, 2, 2, 2, 2, 2, 2})
+
+	empty := Bits32(0)
+	caps := c.addPairs([]Bits32{empty}, 2)
+	fmt.Println(caps)
+
+	roots := c.MinRoots()
+	fmt.Println(len(roots))
+	fmt.Println(roots[0].Root)
+
+	// Output:
+	// [78 278 166 344 232 432]
+	// 1
+	// 78
+}
